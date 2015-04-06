3ecb1737c15a15da399dc805632ce0c3084a61a9
Max Waldt <waldm63@gmail.com>
2015-04-05 21:04:17 -0400

    Create Persisable.cs
    Persistable class to use with database interfaces.

---
+++ Persisable.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermProject {
+    class Persistable {
+        System.Data.OleDb.OleDbConnection conn;
+        protected static string connectionString{get; set;}
+        
+        public Persistable(){
+            conn = new System.Data.OleDb.OleDbConnection();
+        }
+
+        public void configureConnection(){
+            conn.ConnectionString = connectionString;
+        }
+
+        public List<Object> getValues(string queryString){
+            List<Object> results = new List<Object>();
+            configureConnection();
+            using(conn){
+                System.Data.OleDb.OleDbCommand command = new System.Data.OleDb.OleDbCommand(queryString, conn);
+                try{
+                    conn.Open();
+                    System.Data.OleDb.OleDbDataReader reader = command.ExecuteReader();
+                    while(reader.Read()){
+                        Object[] nextRow = new Object[reader.FieldCount];
+                        reader.GetValues(nextRow);
+                        results.Add(nextRow);
+                    }
+                    return results;
+                }catch(Exception e){
+                    Console.WriteLine(e.ToString());
+                    return null;
+                }
+
+            }
+        }
+
+        public int ModifyDatabase(string queryString){
+            configureConnection();
+            using(conn){
+                System.Data.OleDb.OleDbCommand command = new System.Data.OleDb.OleDbCommand(queryString);
+                command.Connection = conn;
+
+                try{
+                    conn.Open();
+                    command.ExecuteNonQuery();
+                    return 0;
+                }
+                catch(Exception ex){
+                    Console.WriteLine(ex.Message);
+                    return 1;
+                }
+            }
+        }
+    }
+}
