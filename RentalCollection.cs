a36b891685ab1695328fc36f8e4484f6de545596
Max Waldt <waldm63@gmail.com>
2015-04-08 00:54:56 -0400

    Create RentalCollection.cs

---
+++ RentalCollection.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermProject {
+    class RentalCollection : Persistable {
+        protected List<Vehicle> rentals;
+
+        public RentalCollection()
+            : base() { rentals = new List<Vehicle>(); } // call parent default constructor
+
+
+        public void PopulateWithGoodAndAvailableBikes() {
+            string queryString = "SELECT ID FROM Vehicle WHERE PhysicalCondition = 'Good' AND Status = 'Active'";
+            List<Object> bikeIds = getValues(queryString);
+            foreach (Object element in bikeIds) {
+                Vehicle v = new Vehicle();
+                v.Populate((int)((object[])element)[0]);
+                rentals.Add(v);
+            }
+
+        }
+
+        public List<Vehicle> GetBikeList() {
+            return rentals;
+        }
+
+        
+        public void PrintAll() {
+            /*
+            foreach(Rental rental in rentals){
+                rental.Print();
+            }
+             */
+        } 
+    }
+}
