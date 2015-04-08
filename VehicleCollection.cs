3a395226a174271364f88d432a7c6e8ee78d707e
Max Waldt <waldm63@gmail.com>
2015-04-08 00:54:21 -0400

    Update VehicleCollection.cs
    Added PrintAll() method.

--- VehicleCollection.cs
+++ VehicleCollection.cs
@@ -4,11 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TermProject
-{
-    class VehicleCollection : Persistable
-    {
-        protected List<Vehicle> bikes;
+namespace TermProject {
+    class VehicleCollection : Persistable {
+        protected List<Vehicle> vehicles;
 
         public VehicleCollection()
             : base() // call parent default constructor
@@ -16,24 +14,28 @@ namespace TermProject
             connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;" +
                 @"Data source= C:\Users\Lisa\Documents" +
                 @"\BicycleRental.accdb";
-            bikes = new List<Vehicle>();
+            vehicles = new List<Vehicle>();
         }
 
-        public void PopulateWithGoodAndAvailableBikes()
-        {
+        public void PopulateWithGoodAndAvailableBikes() {
             string queryString = "SELECT ID FROM Vehicle WHERE PhysicalCondition = 'Good' AND Status = 'Active'";
             List<Object> bikeIds = getValues(queryString);
-            foreach(Object element in bikeIds){
+            foreach (Object element in bikeIds) {
                 Vehicle v = new Vehicle();
-                v.populate((int)((object[])element)[0]);
-                bikes.Add(v);
+                v.Populate((int)((object[])element)[0]);
+                vehicles.Add(v);
             }
 
         }
 
-        public List<Vehicle> GetBikeList()
-        {
-            return bikes;
+        public List<Vehicle> GetBikeList() {
+            return vehicles;
+        }
+
+        public void PrintAll() {
+            foreach(Vehicle vehicle in vehicles){
+                vehicle.Print();
+            }
         }
     }
 }
