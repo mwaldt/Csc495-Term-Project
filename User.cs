366b72b101c37cc71ecb0bb4cac51d92d7007168
Max Waldt <waldm63@gmail.com>
2015-04-08 00:50:54 -0400

    Update User.cs
    Minor changes, to reflect changes made in Persistable

--- User.cs
+++ User.cs
@@ -1,127 +1,138 @@
 using System;
-using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TermProject {
-    class User : Persistable{
+    class User : Persistable {
+        public int ID { get; set; }
+        public string BannerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string UserType { get; set; }
+        public string InitialRegistrationDate { get; private set; }
+        public string Notes { get; set; }
+        public string Status { get; set; }
+        public string DateStatusUpdated { get; set; }
 
-        public string bannerID {set; get;}
-        public string firstName{set; get;}
-        public string lastName{set; get;}
-        public string phoneNumber{set; get;}
-        public string email{set; get;}
-        public string userType{set; get;}
-        public string dateOfRegistration{set; get;}
-        public string notes{set; get;}
-        public string status{set; get;}
-        public string dateStatusUpdated{set; get;}
-        
-        public User() : base() {
-            //This string will changed based on the computers file directory, 
-            connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;" +
-                @"Data source = E:\Workspace\C#\Asgn06 Vehicle\Asgn06 Vehicle\Vehicle.accdb";   //changes based on where you are going to have your working directory
-        }
 
-        public void Populate(int id) {
-            string queryString = "SELECT * FROM [User] WHERE (ID = " + id + ")";
-            ArrayList results = getValues(queryString);
+        public User(): base() { } // call parent default constructor
 
+
+        //------------------------------------------------------------------
+        public void Populate(int ID) {
+            string queryString = "SELECT * FROM [User] WHERE (ID = " + ID + ")";
+            List<Object> results = getValues(queryString);
             if (results != null) {
                 foreach (object result in results) {
-                    IEnumerable row = result as IEnumerable;
+                    IEnumerable<Object> row = result as IEnumerable<Object>;
                     int count = 0;
                     foreach (object rowValue in row) {
+                        // DEBUG Console.WriteLine(rowValue);
                         if (count == 0)
-                            this.bannerID = Convert.ToString(rowValue);
+                            this.ID = Convert.ToInt32(rowValue);
                         else if (count == 1)
-                            this.firstName = Convert.ToString(rowValue);
+                            this.BannerID = Convert.ToString(rowValue);
                         else if (count == 2)
-                            this.lastName = Convert.ToString(rowValue);
+                            FirstName = Convert.ToString(rowValue);
                         else if (count == 3)
-                            this.phoneNumber = Convert.ToString(rowValue);
+                            LastName = Convert.ToString(rowValue);
                         else if (count == 4)
-                            this.email = Convert.ToString(rowValue);
+                            PhoneNumber = Convert.ToString(rowValue);
                         else if (count == 5)
-                            this.userType = Convert.ToString(rowValue);
+                            Email = Convert.ToString(rowValue);
                         else if (count == 6)
-                            this.dateOfRegistration = Convert.ToString(rowValue);
+                            UserType = Convert.ToString(rowValue);
                         else if (count == 7)
-                            this.notes = Convert.ToString(rowValue);
+                            InitialRegistrationDate = Convert.ToString(rowValue);
                         else if (count == 8)
-                            this.status = Convert.ToString(rowValue);
+                            Notes = Convert.ToString(rowValue);
                         else if (count == 9)
-                            this.dateStatusUpdated = Convert.ToString(rowValue);
-                        count++;
+                            Status = Convert.ToString(rowValue);
+                        else if (count == 10)
+                            DateStatusUpdated = Convert.ToString(rowValue);
+                        count = count + 1;
                     }
                 }
             }
         }
 
+        //------------------------------------------------------------------
         public void Insert() {
-            string query = "INSERT INTO [User] (BannerID, FirstName, LastName, PhoneNumber, Email, UserType, DateOfRegistration, Notes, Status, DateStatusUpdated) VALUES ('" +
-                this.bannerID + "', '" + this.firstName + "', '" + this.lastName + "', '" + this.phoneNumber + "', '" + this.email + "', '" + this.userType
-                + "', '" + this.dateOfRegistration + "', '" + this.notes + "', '" + this.status + "', '" + this.dateStatusUpdated + "')";
-            int returnCode = ModifyDatabase(query);
+            this.InitialRegistrationDate = DateTime.Now.ToString("yyyy-MM-dd");
+            string insertQuery =
+            "INSERT INTO [User] (BannerId, FirstName, LastName, PhoneNumber, EmailAddress, UserType, InitialRegistrationDate, Notes, Status, DateStatusUpdated) " +
+            "VALUES (" +
+            "'" + this.BannerID + "', '" +
+            this.FirstName + "', '" +
+            this.LastName + "', '" +
+            this.PhoneNumber + "', '" +
+            this.Email + "', '" +
+            this.UserType + "', '" +
+            this.InitialRegistrationDate + "', '" +
+            this.Notes + "', '" +
+            this.Status + "', '" +
+            this.DateStatusUpdated + "')";
+            int returnCode = ModifyDatabase(insertQuery);
             if (returnCode != 0) {
-                Console.WriteLine("Error in inserting User into Database");
+                Console.WriteLine("Error in inserting Worker object into database");
             } else {
-                Console.WriteLine("User object successfuly inserted");
+                Console.WriteLine("Worker object successfully inserted");
                 string idQueryString = "SELECT MAX(ID) FROM [User]";
-                ArrayList results = getValues(idQueryString);
+                List<Object> results = getValues(idQueryString);
                 if (results != null) {
+                    // DEBUG Console.WriteLine("Got an id from id query");
                     foreach (object result in results) {
-                        IEnumerable row = result as IEnumerable;
+                        IEnumerable<Object> row = result as IEnumerable<Object>;
                         foreach (object rowValue in row) {
-                            this.id = Convert.ToInt32(rowValue);
-                            this.id++;
+                            // DEBUG Console.WriteLine("Retrieved id = " + rowValue);
+                            this.ID = Convert.ToInt32(rowValue);
                         }
                     }
                 }
             }
         }
 
+        //------------------------------------------------------------------
         public void Update() {
-            string updateQuery = "UPDATE [User] SET" +
-                " FirstName " + this.firstName +
-                " LastName " + this.lastName +
-                " PhoneNumber " + this.phoneNumber +
-                " Email " + this.email +
-                " UserType " + this.userType +
-                " DateOfRegistration " + this.dateOfRegistration +
-                " Notes " + this.notes +
-                " Status " + this.status +
-                " DateStatusUpdated " + this.dateStatusUpdated +
-                " WHERE ID = " + this.bannerID;
+            string updateQuery = "UPDATE Worker SET " +
+                " BannerId = '" + this.BannerID + "' ," +
+                " FirstName = '" + this.FirstName + "' ," +
+                " LastName = '" + this.LastName + "' ," +
+                " PhoneNumber = '" + this.PhoneNumber + "' ," +
+                " EmailAddress = '" + this.Email + "' ," +
+                " UserType = '" + this.UserType + "', " +
+                " Notes = '" + this.Notes + "', " +
+                " Status = '" + this.Status + "', " +
+                " DateStatusUpdated = '" + this.DateStatusUpdated + "' " +
+                " WHERE " +
+                " ID = " + this.ID;
             int returnCode = ModifyDatabase(updateQuery);
             if (returnCode != 0)
-                Console.WriteLine("Error in updating user object in database");
+                Console.WriteLine("Error in updating Worker object into database");
             else
-                Console.WriteLine("User successfully updated");
+                Console.WriteLine("Worker object successfully updated");
         }
 
+        //------------------------------------------------------------------
         public void Delete() {
-            string updateQuery = "DELETE FROM [User] WHERE ID = " + this.id;
-            int returnCode = ModifyDatabase(updateQuery);
+            string deleteQuery = "DELETE FROM Worker WHERE " +
+                " ID = " + this.ID;
+            Console.WriteLine(deleteQuery);
+            int returnCode = ModifyDatabase(deleteQuery);
             if (returnCode != 0)
-                Console.WriteLine("Error in deleting User object in database");
+                Console.WriteLine("Error in deleting Worker object from database");
             else
-                Console.WriteLine("User successfully deleted");
+                Console.WriteLine("Worker object successfully deleted");
         }
 
-        public void PrintUser() {
-            string printString = "************" +
-                "\nBanner ID: " + this.bannerID +
-                "\nFirst Name: " + this.firstName +
-                "\nLast Name: " + this.lastName +
-                "\nPhone Number: " + this.phoneNumber +
-                "\nEmail Address: " + this.email +
-                "\nUser Type: " + this.userType +
-                "\nDate Of Registration: " + this.DateOfRegistration +
-                "\nNotes: " + this.notes +
-                "\nStatus: " + this.status + "\n";
-            Console.WriteLine(printString);
+        public override string ToString() {
+            return "ID:\t\t\t" + this.ID + "\nBanner Id:\t\t" + this.BannerID + "\nFirst name:\t\t" + this.FirstName + "\nLast Name:\t\t" + this.LastName + "\nPhone number:\t\t" + this.PhoneNumber +
+                "\nEmail address:\t\t" + this.Email + "\nUser Type:\t\t" + this.UserType + "\nRegistration date:\t" + this.InitialRegistrationDate + "\nNotes:\t\t\t" + this.Notes +
+                "\nStatus:\t\t\t" + this.Status + "\nDate status updated:\t" + this.DateStatusUpdated;
         }
     }
 }
