cd827333ad818ec4e5aae87889277d9e51324d5a
Max Waldt <waldm63@gmail.com>
2015-04-10 01:12:09 -0400

    Update Persistable.cs
    Idiot proofing push

--- Persistable.cs
+++ Persistable.cs
@@ -1,3 +1,9 @@
+/* Persistable.cs
+ * Max Waldt & Lisa Moore
+ * Description: The super class for all persistable objects to be inserted into the database.
+ * Contains all methods for interaction with the database.
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Collections;
@@ -8,36 +14,40 @@ using System.Threading.Tasks;
 namespace TermProject {
     class Persistable {
         System.Data.OleDb.OleDbConnection conn;
-        protected static string connectionString{get; set;}
-        
-        public Persistable(){
+        protected static string connectionString { get; set; }
+
+        //Default Constructor
+        public Persistable() {
             conn = new System.Data.OleDb.OleDbConnection();
             connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;" +
                 //@"Data source = C:\Users\Lisa\Documents" +                                             //Lisa laptop?
-                //@"Data source = E:\Workspace\C#\TermProject\TermProject" +                              //Max Desktop
-                @"Data source = C:\Users\Maximus\Documents\Visual Studio 2013\Projects\TermProject\TermProject" +            //Max laptop, will fill in later
+                @"Data source = E:\Workspace\C#\TermProject\TermProject" +                              //Max Desktop
+                //@"Data source = C:\Users\Maximus\Documents\Visual Studio 2013\Projects\TermProject\TermProject" +            //Max laptop, will fill in later
                 @"\BicycleRental.accdb";
         }
 
-        public void configureConnection(){
+        //Sets up connection to the connection to the database
+        public void configureConnection() {
             conn.ConnectionString = connectionString;
         }
 
-        public List<Object> getValues(string queryString){
+        //Queries the database to retrieve results based on the parameter queryString
+        //Returns the List<Object> of all results
+        public List<Object> getValues(string queryString) {
             List<Object> results = new List<Object>();
             configureConnection();
-            using(conn){
+            using (conn) {
                 System.Data.OleDb.OleDbCommand command = new System.Data.OleDb.OleDbCommand(queryString, conn);
-                try{
+                try {
                     conn.Open();
                     System.Data.OleDb.OleDbDataReader reader = command.ExecuteReader();
-                    while(reader.Read()){
+                    while (reader.Read()) {
                         Object[] nextRow = new Object[reader.FieldCount];
                         reader.GetValues(nextRow);
                         results.Add(nextRow);
                     }
                     return results;
-                }catch(Exception e){
+                } catch (Exception e) {
                     Console.WriteLine(e.ToString());
                     return null;
                 }
@@ -45,27 +55,28 @@ namespace TermProject {
             }
         }
 
-        public int ModifyDatabase(string queryString){
+        //Method to test connection to database absed on the parameter query
+        public int ModifyDatabase(string queryString) {
             configureConnection();
-            using(conn){
+            using (conn) {
                 System.Data.OleDb.OleDbCommand command = new System.Data.OleDb.OleDbCommand(queryString);
                 command.Connection = conn;
 
-                try{
+                try {
                     conn.Open();
                     command.ExecuteNonQuery();
                     return 0;
-                }
-                catch(Exception ex){
+                } catch (Exception ex) {
                     Console.WriteLine(ex.Message);
                     return 1;
                 }
             }
         }
 
+        //Returns the current date as a string, used when database updates.
         private string SetDateUpdated() { return DateTime.Now.ToString("yyyy-MM-dd"); }
 
     }
 
-    
+
 }
